3de2d23b3d22bc223695e2f3dda91df8f56f4685
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-09 16:43:00 +0530

    arithmetic calculation

---
+++ 100Day Challenge/Day 2/script.py
@@ -0,0 +1,25 @@
+# day 2 datatypes string,integer,float,boolean.
+# subscripting
+print("Hello"[0])
+string_type = "string"
+
+
+# interger
+# below is 34 thousands as the compiler will remove
+# underscore and consider it to be whole
+numone = 34_234
+numtwo = 235
+float_one = float(numone)
+
+print(numone + numtwo+float_one)
+converted_num = str(numone)
+
+booleanValue = False
+print(type(booleanValue), type(numone), type(string_type), type(converted_num))
+
+
+# Arithmetic opetation order PEDMAS (left to right)
+# p-parentheses ()
+# exponent **
+#  multiplication,division * /
+# addition,subtraction +,l
