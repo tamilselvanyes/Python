cdf809ce2fffa25040add4e62bc459c04a830a6b
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-14 17:16:36 +0530

    day 8 started

---
+++ 100Day Challenge/Day 8/script.py
@@ -0,0 +1,13 @@
+# function:
+def greet_with_name(name, location):
+    print(f"Good morning {name} from {location}")
+
+
+# positional arguments:
+greet_with_name("Tamil", "Mettur")
+
+# keyword arguments:
+greet_with_name(location="Tamil", name="Mettur")
+
+# parameter is the name like "name, location" in the previous example
+# argument is the value like "Tamil, Mettur" in the previous example
