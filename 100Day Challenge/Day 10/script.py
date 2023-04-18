56b2e19655e9f380e7475b1037cce10932815422
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-18 08:04:47 +0530

    function with output

---
+++ 100Day Challenge/Day 10/script.py
@@ -0,0 +1,11 @@
+# function with output
+
+def multiply(a, b):
+    return a*b
+
+
+def format_name(first_name, last_name):
+    return (f"{first_name.title()} {last_name.title()}")
+
+
+print(format_name("tamil", "sElvan"))
