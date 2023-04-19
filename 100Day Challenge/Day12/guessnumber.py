3069d3cf58f2ef94cad06c8d67470b514f5fc8ae
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-19 08:33:04 +0530

    number guessing complete

---
+++ 100Day Challenge/Day12/guessnumber.py
@@ -0,0 +1,32 @@
+import random
+print("Welcome to the Number Guessing Game")
+print("I am thinkng of a number between 1 and 100")
+difficulty = input("Choose difficulty. Type 'easy' or 'hard':")
+
+no_of_attempts = 5
+no_of_guesses = 0
+game_won = False
+correct_number = random.randint(1, 101)
+
+if (difficulty == 'easy'):
+    no_of_attempts = 10
+
+while (no_of_attempts > 0 and not game_won):
+    if (no_of_guesses != 0):
+        print("Guess again")
+    print(f"You have {no_of_attempts} attempts to guess the number")
+    guess = int(input("Make a guess: "))
+    if (guess == correct_number):
+        print(
+            f"You got it in {no_of_guesses} guesses! The answer was {correct_number}")
+        game_won = True
+    elif (guess > correct_number):
+        print("Too high")
+        no_of_attempts -= 1
+    else:
+        print("Too low")
+        no_of_attempts -= 1
+    no_of_guesses += 1
+
+if (no_of_attempts == 0):
+    print("You've run out of guesses, you lose")
