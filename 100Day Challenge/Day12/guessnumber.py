282d1504d691e07302a7898fa7ee8906deac964d
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-19 08:54:23 +0530

    code optimisation

--- 100Day Challenge/Day12/guessnumber.py
+++ 100Day Challenge/Day12/guessnumber.py
@@ -1,35 +1,47 @@
 import random
 from art import logo
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 print(logo)
 print("Welcome to the Number Guessing Game")
 print("I am thinkng of a number between 1 and 100")
-difficulty = input("Choose difficulty. Type 'easy' or 'hard':")
 
-no_of_attempts = 5
-no_of_guesses = 0
-game_won = False
-correct_number = random.randint(1, 101)
 
-if (difficulty == 'easy'):
-    no_of_attempts = 10
-
-while (no_of_attempts > 0 and not game_won):
-    if (no_of_guesses != 0):
-        print("Guess again")
-    print(f"You have {no_of_attempts} attempts to guess the number")
-    guess = int(input("Make a guess: "))
-    if (guess == correct_number):
-        print(
-            f"You got it in {no_of_guesses} guesses! The answer was {correct_number}")
-        game_won = True
-    elif (guess > correct_number):
-        print("Too high")
-        no_of_attempts -= 1
+def set_difficulty():
+    difficulty = input("Choose difficulty. Type 'easy' or 'hard':")
+    if (difficulty == "easy"):
+        return EASY_LEVEL_TURNS
     else:
-        print("Too low")
-        no_of_attempts -= 1
-    no_of_guesses += 1
+        return HARD_LEVEL_TURNS
 
-if (no_of_attempts == 0):
-    print("You've run out of guesses, you lose")
+
+def play_game():
+    print("Welcome to the Number Guessing Game")
+    print("I am thinkng of a number between 1 and 100")
+    correct_number = random.randint(1, 100)
+    no_of_attempts = set_difficulty()
+    no_of_guesses = 0
+
+    while (no_of_attempts > 0):
+        if (no_of_guesses != 0):
+            print("Guess again")
+        print(f"You have {no_of_attempts} attempts to guess the number")
+        guess = int(input("Make a guess: "))
+        if (guess == correct_number):
+            print(
+                f"You got it in {no_of_guesses} guesses! The answer was {correct_number}")
+            return
+        elif (guess > correct_number):
+            print("Too high")
+            no_of_attempts -= 1
+        else:
+            print("Too low")
+            no_of_attempts -= 1
+        no_of_guesses += 1
+    if (no_of_attempts == 0):
+        print("You've run out of guesses, you lose")
+
+
+play_game()
