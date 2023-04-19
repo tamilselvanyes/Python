63c8c6c0cb77dd4ad9bb7210eabc6d84520d0b35
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-19 10:08:08 +0530

    day 13 completed

---
+++ 100Day Challenge/Day13/script.py
@@ -0,0 +1,10 @@
+# debugging tips
+# Describe a bug
+# Reproduce the bug
+# Print statements
+# Act as a computer
+# Use debugging tools
+# Take a break
+# Ask a friend
+# Run often
+# Ask stackflow
