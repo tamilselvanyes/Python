65c6ff731cde994b53b71356fc28e32f78d4d01b
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-21 23:59:40 +0530

    day 16 progress OOPs

---
+++ 100Day Challenge/Day16/sample_module.py
@@ -0,0 +1,2 @@
+sample_variable = 10
+
---
+++ 100Day Challenge/Day16/script.py
@@ -0,0 +1,13 @@
+import sample_module
+from turtle import Turtle, Screen
+
+
+print(sample_module.sample_variable)
+
+timmy = Turtle()
+
+print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
