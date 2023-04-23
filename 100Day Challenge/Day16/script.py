cb10e4d1ec94a7ba07440984ce1f07a94ece9186
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-23 11:32:18 +0530

    day 16 progress OOPs

--- 100Day Challenge/Day16/script.py
+++ 100Day Challenge/Day16/script.py
@@ -5,9 +5,16 @@ from turtle import Turtle, Screen
 print(sample_module.sample_variable)
 
 timmy = Turtle()
-
+timmy.shape("turtle")
+timmy.color("red")
+timmy.circle(100)
 print(timmy)
+
 
 my_screen = Screen()
 print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
 
