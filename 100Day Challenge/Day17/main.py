88bbd13d8be2bea2a0fabafc7f2e087ee36306bf
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-23 23:21:41 +0530

    day 17 started

---
+++ 100Day Challenge/Day17/main.py
@@ -0,0 +1,4 @@
+# if you don't want to give anything under the function you can give pass
+# PascalCase- first letter capital
+# camelCase- first letter small
+# snake_case- all small but separated by "_"
