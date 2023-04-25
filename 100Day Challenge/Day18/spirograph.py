b4a3769b6ecf76fc19acd174fa9061ffb758a6ed
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-25 09:03:40 +0530

    spirograph implementation

--- 100Day Challenge/Day18/spirograph.py
+++ 100Day Challenge/Day18/spirograph.py
@@ -1,10 +1,8 @@
 from turtle import Turtle, Screen
 import turtle as t
 import random
-from colors import colors
 
 directions = [0, 90, 180, 270]
-widths = [5, 6, 7, 8, 9, 10]
 turtle = Turtle()
 turtle.shape("arrow")
 t.colormode(255)
@@ -14,17 +12,15 @@ def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    my_tuple = (r, g, b)
-    return my_tuple
+    color = (r, g, b)
+    return color
 
 
-for _ in range(0, 100):
-    turtle.forward(30)
+for item in range(0, 50):
+    turtle.circle(100)
     turtle.speed("fastest")
     turtle.color(random_color())
-    turtle.width(random.choice(widths))
-    turtle.color(random.choice(colors))
-    turtle.setheading(random.choice(directions))
+    turtle.left(item)
 
 screen = Screen()
 screen.exitonclick()
