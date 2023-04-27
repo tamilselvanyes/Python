33f1367a3c2ad6c4d421a5947087a295c64a6372
tamilselvanyes <tamilselvanyes@gmail.com>
2023-04-28 00:26:42 +0530

    scoreboard constants update.

--- 100Day Challenge/Day20/scoreboard.py
+++ 100Day Challenge/Day20/scoreboard.py
@@ -1,16 +1,17 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Arial', 18, 'normal')
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.begin_fill()
         self.penup()
-        self.pencolor("white")
+        self.color("white")
         self.goto(0, 280)
         self.hideturtle()
         self.update_score()
 
     def update_score(self):
         self.clear()
-        self.write(f"Score:{self.score}", False, align="center", font=('Arial', 18, 'normal'))
+        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
