64d446bdb3c3dbd66787aa5f4a192b871f530dd1
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-02 23:57:40 +0530

    moving cars

--- 100Day Challenge/Day23/turtle-crossing/car_manager.py
+++ 100Day Challenge/Day23/turtle-crossing/car_manager.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+INITAL_CAR_POSITION = [(280,-240), (280,-160), (280,-80), (280,0), (280,80), (280,160)]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
@@ -8,11 +9,14 @@ MOVE_INCREMENT = 10
 class CarManager:
     def __init__(self):
         self.cars = []
-        for car in COLORS:
+        for index,car in enumerate(COLORS):
             turtle_new = Turtle()
             turtle_new.color(car)
             turtle_new.shape("square")
             turtle_new.shapesize(1, 2)
-            turtle_new.goto(turtle_new.xcor() + MOVE_INCREMENT, turtle_new.ycor())
+            turtle_new.goto(INITAL_CAR_POSITION[index])
             self.cars.append(turtle_new)
 
+    def move_cars_forward(self):
+        for car in self.cars:
+            car.goto(car.xcor() - MOVE_INCREMENT,car.ycor())
--- 100Day Challenge/Day23/turtle-crossing/main.py
+++ 100Day Challenge/Day23/turtle-crossing/main.py
@@ -13,6 +13,7 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.move_cars_forward()
 
 
 screen.exitonclick()
