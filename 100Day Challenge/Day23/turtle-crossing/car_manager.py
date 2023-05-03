f786bb1d6dd5401abc9be538b089aff244b4a73f
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-03 22:46:57 +0530

    cars movement implementation

--- 100Day Challenge/Day23/turtle-crossing/car_manager.py
+++ 100Day Challenge/Day23/turtle-crossing/car_manager.py
@@ -1,26 +1,20 @@
+import random
 from turtle import Turtle
 from random import randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-INITAL_CAR_POSITION = [(280, -200), (280, -120), (280, -40), (280, 40), (280, 120), (280, 200)]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
     def __init__(self):
-        self.cars = []
-        for index, car in enumerate(COLORS):
-            turtle_new = Turtle()
-            turtle_new.color(car)
-            turtle_new.shape("square")
-            turtle_new.penup()
-            print(randint(1, 10), "SPEED")
-            turtle_new.speed(randint(1, 10))
-            turtle_new.shapesize(1, 2)
-            turtle_new.goto(INITAL_CAR_POSITION[index])
-            self.cars.append(turtle_new)
+        self.car = Turtle()
+        self.car.color(random.choice(COLORS))
+        self.car.shape("square")
+        self.car.penup()
+        self.car.goto(280, randint(-240, 240))
+        self.car.shapesize(1, 2)
 
     def move_cars_forward(self):
-        for car in self.cars:
-            car.goto(car.xcor() - MOVE_INCREMENT, car.ycor())
+        self.car.goto(self.car.xcor() - MOVE_INCREMENT, self.car.ycor())
--- 100Day Challenge/Day23/turtle-crossing/main.py
+++ 100Day Challenge/Day23/turtle-crossing/main.py
@@ -7,13 +7,17 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-car_manager = CarManager()
+car_managers = []
+
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.3)
     screen.update()
-    car_manager.move_cars_forward()
+    car_manager_new = CarManager()
+    car_managers.append(car_manager_new)
+    for car_manager in car_managers:
+        car_manager.move_cars_forward()
 
 
 screen.exitonclick()
