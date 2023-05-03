9b5c17dd980493bc2b625bb5cafda7e46677b2f2
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-03 22:58:16 +0530

    player movement implementation

--- 100Day Challenge/Day23/turtle-crossing/main.py
+++ 100Day Challenge/Day23/turtle-crossing/main.py
@@ -7,8 +7,14 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
 car_managers = []
+player = Player()
 
+screen.onkey(player.move_up, "Up")
+screen.onkey(player.move_down, "Down")
+screen.onkey(player.move_left(), "Left")
+screen.onkey(player.move_right, "Right")
 
 game_is_on = True
 while game_is_on:
--- 100Day Challenge/Day23/turtle-crossing/player.py
+++ 100Day Challenge/Day23/turtle-crossing/player.py
@@ -1,7 +1,29 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.shape("turtle")
+        self.setheading(90)
+
+    def move_up(self):
+        self.goto(self.xcor(), self.ycor() + MOVE_DISTANCE)
+
+    def move_down(self):
+        self.goto(self.xcor() , self.ycor() - MOVE_DISTANCE)
+
+    def move_right(self):
+        self.goto(self.xcor() + MOVE_DISTANCE, self.ycor())
+
+    def move_left(self):
+        self.goto(self.xcor() - MOVE_DISTANCE, self.ycor())
+
+
