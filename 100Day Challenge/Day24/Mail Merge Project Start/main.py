472c860b6e17f2279beefe00610ece71a67a1313
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-04 23:54:12 +0530

    Invitation created for all my friends

--- 100Day Challenge/Day24/Mail Merge Project Start/main.py
+++ 100Day Challenge/Day24/Mail Merge Project Start/main.py
@@ -1,8 +1,13 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt") as letter:
+    content = letter.read()
+
+
+with open("./Input/Names/invited_names.txt") as names:
+    names_list = names.read().split('\n')
+    for name in names_list:
+        modified_content = content.replace("[name]", name)
+        with open(f"./Output/ReadyToSend/{name}.txt", mode="w") as new_letter:
+            new_letter.write(modified_content)
+
+
