1d2c436dd27b776699a491e32e242080bc3c5a29
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-05 00:05:27 +0530

    added comments and optimisation

--- 100Day Challenge/Day24/Mail Merge Project Start/main.py
+++ 100Day Challenge/Day24/Mail Merge Project Start/main.py
@@ -1,12 +1,13 @@
-
+PLACE_HOLDER = "[name]"
 with open("./Input/Letters/starting_letter.txt") as letter:
     content = letter.read()
 
 
 with open("./Input/Names/invited_names.txt") as names:
     names_list = names.read().split('\n')
+    # can also use readlines() method
     for name in names_list:
-        modified_content = content.replace("[name]", name)
+        modified_content = content.replace(PLACE_HOLDER, name)
         with open(f"./Output/ReadyToSend/{name}.txt", mode="w") as new_letter:
             new_letter.write(modified_content)
 
