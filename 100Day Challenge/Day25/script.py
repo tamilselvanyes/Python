5d78ba4c8d9733599f874f5d2832558734d54cb0
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-05 23:49:26 +0530

    introduction to pandas

--- 100Day Challenge/Day25/script.py
+++ 100Day Challenge/Day25/script.py
@@ -2,13 +2,18 @@
 #     data = weather_data.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as weather_data:
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#
+# print(temperatures)
 
-with open("weather_data.csv") as weather_data:
-    data = csv.reader(weather_data)
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
+import pandas
 
-print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
