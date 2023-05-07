bbeb0bcfc3ea51dcda2221c9194e71b1a97e93d8
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-07 12:22:14 +0530

    day 25 completed

---
+++ 100Day Challenge/Day25/us_state_game/main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. STATES QUIZ")
+IMAGE = "blank_states_img.gif"
+guessed_states = []
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+game_is_on = True
+data = pandas.read_csv("50_states.csv")
+states_list = data["state"].to_list()
+
+while game_is_on:
+    answer_state = screen.textinput(title=f"Guess state {len(guessed_states)}/50",
+                                    prompt="Please input your state name")
+    if answer_state:
+        write_turtle = turtle.Turtle()
+        write_turtle.hideturtle()
+        title_case = answer_state.title()
+        if title_case in states_list:
+            required_state = data[data.state == title_case]
+            write_turtle.penup()
+            write_turtle.goto(int(required_state.x), int(required_state.y))
+            write_turtle.write(f"{title_case}", align="center", font=('Arial', 14, 'normal'))
+            guessed_states.append(title_case)
+            if len(guessed_states) == 50:
+                game_is_on = False
+    else:
+        game_is_on = False
+
+states_to_learn = []
+
+for state in states_list:
+    if state not in guessed_states:
+        states_to_learn.append(state)
+
+df = pandas.DataFrame(states_to_learn)
+df.to_csv("learning.csv")
