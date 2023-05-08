85787d0bcef62daa55f13c08575670dc3909e478
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-08 23:47:58 +0530

    entry component

--- 100Day Challenge/Day27/main.py
+++ 100Day Challenge/Day27/main.py
@@ -12,10 +12,13 @@ my_label.pack()
 
 
 def button_click_handler():
-    my_label.config(text="Button is clicked")
+    my_label.config(text= input_entry.get())
 
 
 button = Button(text="Click me", command=button_click_handler)
 button.pack()
+
+input_entry = Entry()
+input_entry.pack()
 
 window.mainloop()
