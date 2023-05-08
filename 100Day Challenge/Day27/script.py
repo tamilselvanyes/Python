2ad7bc428b48fbc7d3a31f33f853e9f3ecf13688
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-08 23:19:06 +0530

    args and kwargs

--- 100Day Challenge/Day27/script.py
+++ 100Day Challenge/Day27/script.py
@@ -1,9 +1,18 @@
 # this function takes in any number of arguments
 def add(*args):
     sum_value = 0
+    # type tuple
     for n in args:
         sum_value += n
     return sum_value
 
 
 print(add(1, 2, 3, 4, 5, 6))
+
+
+def calculate(**kwargs):
+    # type dictionary
+    print(kwargs)
+
+
+calculate(add=3, multiply=5)
