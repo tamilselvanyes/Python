6607c48687ee1d0bf9009047107dba4ec34bda84
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-09 08:26:16 +0530

    count down mechanism

--- 100Day Challenge/Day28/pomodoro-project/main.py
+++ 100Day Challenge/Day28/pomodoro-project/main.py
@@ -1,5 +1,7 @@
-
 # ---------------------------- CONSTANTS ------------------------------- #
+from tkinter import *
+import math
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -9,10 +11,55 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(WORK_MIN * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    print(count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("POMODORO")
+window.config(padx=100, pady=100)
+
+canvas = Canvas(width=200, height=224)
+
+photo_image = PhotoImage(file="tomato.png")
+
+canvas.create_image(103, 112, image=photo_image)
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+timer_label = Label()
+timer_label.config(text="Timer", font=(FONT_NAME, 70))
+timer_label.grid(row=0, column=1)
+
+start_button = Button()
+start_button.config(text="Start", command=start_timer)
+start_button.grid(row=2, column=0)
+
+reset_button = Button()
+reset_button.config(text="Reset")
+reset_button.grid(row=2, column=2)
+
+check_mark = Label()
+check_mark.config(text="✔", fg=GREEN)
+check_mark.grid(row=3, column=1)
+
+
+window.mainloop()
