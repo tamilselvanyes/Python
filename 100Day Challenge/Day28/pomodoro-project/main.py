7276222e8ec1b403cd53c67e5856a42e7f3f5af9
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-09 09:41:04 +0530

    day 28 completed

--- 100Day Challenge/Day28/pomodoro-project/main.py
+++ 100Day Challenge/Day28/pomodoro-project/main.py
@@ -10,25 +10,67 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    global timer
+    global reps
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="TIMER")
+    check_mark.config(text="")
+    reps = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 2 != 0:
+        timer_label.config(text="WORK", fg=GREEN)
+        count_down(work_sec)
+
+    elif reps % 8 == 0:
+        timer_label.config(text="LONG BREAK", fg=RED)
+        count_down(long_break_sec)
+    else:
+        timer_label.config(text="SHORT BREAK", fg=PINK)
+        count_down(short_break_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def count_down(count):
-    print(count)
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    global reps
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    if count_min < 10:
+        count_min = f"0{count_min}"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            mark += "✔"
+            check_mark.config(text=mark)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -54,12 +96,11 @@ start_button.config(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button()
-reset_button.config(text="Reset")
+reset_button.config(text="Reset", command=reset)
 reset_button.grid(row=2, column=2)
 
 check_mark = Label()
-check_mark.config(text="✔", fg=GREEN)
+check_mark.config(text="", fg=GREEN)
 check_mark.grid(row=3, column=1)
-
 
 window.mainloop()
