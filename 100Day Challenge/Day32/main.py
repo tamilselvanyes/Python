2bd5d1c31e2d8817e595552706c7e8a0151b33b8
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-11 23:25:25 +0530

    Added subject to the mail

--- 100Day Challenge/Day32/main.py
+++ 100Day Challenge/Day32/main.py
@@ -7,10 +7,10 @@ my_email = os.getenv("EMAIL")
 my_password = os.getenv("EMAIL_PASSWORD")
 test_email = os.getenv("TEST_EMAIL")
 # making connection with the smpt
-connection = smtplib.SMTP("smtp.gmail.com")
-# tls -> transport layer security, makes the connection secure
-connection.starttls()
-connection.login(my_email,my_password)
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    # tls -> transport layer security, makes the connection secure
+    connection.starttls()
+    connection.login(my_email,my_password)
 
-connection.sendmail(from_addr=my_email, to_addrs= test_email,
-                    msg="Hello this is testing mail")
+    connection.sendmail(from_addr=my_email, to_addrs= test_email,
+                        msg="Subject:Subject\n\nHello this is testing mail")
