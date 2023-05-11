29b4444683f66e695020edb02fdcbbd02a2669c6
tamilselvanyes <tamilselvanyes@gmail.com>
2023-05-11 23:18:39 +0530

    Email test complete

--- 100Day Challenge/Day32/main.py
+++ 100Day Challenge/Day32/main.py
@@ -3,8 +3,14 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+my_email = os.getenv("EMAIL")
+my_password = os.getenv("EMAIL_PASSWORD")
+test_email = os.getenv("TEST_EMAIL")
 # making connection with the smpt
 connection = smtplib.SMTP("smtp.gmail.com")
 # tls -> transport layer security, makes the connection secure
 connection.starttls()
-connection.login(os.getenv("EMAIL"), os.getenv("EMAIL_PASSWORD"))
+connection.login(my_email,my_password)
+
+connection.sendmail(from_addr=my_email, to_addrs= test_email,
+                    msg="Hello this is testing mail")
